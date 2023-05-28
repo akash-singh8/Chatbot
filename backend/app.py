678e44d6d0b8deeffb0f3b0273278183a6224e18
akash-singh8 <developer.akash8@gmail.com>
2023-05-28 14:45:52 +0530

    server - listen for post request

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from chat import get_response
+
+app = Flask(__name__)
+CORS(app)
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    if (text):
+        response = get_response(text)
+        message = {"answer": response}
+    else:
+        message = {"answer": "Invalid request!"}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
